d22a8dd6c0fd408c37de15619fd9378821c7739f
Nishanth Murugan <nishanth.murugan@gmail.com>
2020-08-10 21:42:36 +0530

    additional changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,147 @@
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audio_cache.dart';
+import 'quiz_brain.dart';
 
-void main() => runApp(XylophoneApp());
+QuizBrain quizBrain = QuizBrain();
 
-class XylophoneApp extends StatelessWidget {
-  void playSound(int note) {
-    final player = AudioCache();
-    player.play('note$note.wav');
-  }
+void main() => runApp(Quizzler());
 
-  Expanded buildKey(int num, MaterialColor color) {
-    return Expanded(
-      child: FlatButton(
-        color: color,
-        onPressed: () {
-          playSound(num);
-        },
-      ),
-    );
-  }
-
+class Quizzler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
         body: SafeArea(
-          child: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                buildKey(1, Colors.red),
-                buildKey(2, Colors.green),
-                buildKey(3, Colors.blue),
-                buildKey(4, Colors.yellow),
-                buildKey(5, Colors.orange),
-                buildKey(6, Colors.grey),
-                buildKey(7, Colors.teal),
-              ],
-            ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
           ),
         ),
       ),
     );
   }
 }
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Widget> scoreKeeper = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrain.getQuestionText(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                var correctAnswer = quizBrain.getQuestionAnswer();
+
+                if (correctAnswer == true) {
+                  setState(() {
+                    quizBrain.nextQuestion();
+                    scoreKeeper.add(
+                      Icon(
+                        Icons.check,
+                        color: Colors.green,
+                      ),
+                    );
+                  });
+                } else {
+                  setState(() {
+                    quizBrain.nextQuestion();
+                    scoreKeeper.add(
+                      Icon(
+                        Icons.close,
+                        color: Colors.red,
+                      ),
+                    );
+                  });
+                }
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                var correctAnswer = quizBrain.getQuestionAnswer();
+                if (correctAnswer == false) {
+                  setState(() {
+                    quizBrain.nextQuestion();
+                    scoreKeeper.add(
+                      Icon(
+                        Icons.check,
+                        color: Colors.green,
+                      ),
+                    );
+                  });
+                } else {
+                  setState(() {
+                    quizBrain.nextQuestion();
+                    scoreKeeper.add(
+                      Icon(
+                        Icons.close,
+                        color: Colors.red,
+                      ),
+                    );
+                  });
+                }
+              },
+            ),
+          ),
+        ),
+        Row(
+          children: scoreKeeper,
+        )
+      ],
+    );
+  }
+}
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
